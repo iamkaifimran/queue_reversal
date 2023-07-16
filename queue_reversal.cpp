f08b0aaf150a02aa6edcb804b4b956ef61bea74b
kaif <kaifimran0786@gmail.com>
2023-07-16 22:47:05 +0530

    first

---
+++ queue_reversal.cpp
@@ -0,0 +1,23 @@
+
+
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+   stack<int>s;
+   while(!q.empty())
+   {
+       s.push(q.front());
+       q.pop();
+       
+   }
+   while(!s.empty())
+   {
+       q.push(s.top());
+       s.pop();
+       
+   }
+   return q;
+   
+}
